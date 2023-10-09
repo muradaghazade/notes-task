babfe6d20adcc463c5d8e436f98505e5cf278d7c
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2023-10-09 17:26:47 +0400

    first commit

---
+++ database.py
@@ -0,0 +1,14 @@
+notes = [
+    {
+        "title": "First Note",
+        "text": "Ya pervaya zametkaa"
+    },
+    {
+        "title": "Second Note",
+        "text": "Ya vtoraya zametkaa"
+    },
+    {
+        "title": "Third Note",
+        "text": "Ya tretya zametkaa"
+    },
+]
---
+++ main.py
@@ -0,0 +1,11 @@
+#add_note - Jamal
+
+#delete_note - Jamil
+
+#edit_note_text -Jamal
+
+#rename_name -Jamil
+
+#show_all_notes -Jamal
+
+#start - Together
